a7739dbc56faf4065f36907f04f7b477c124dd95
Jack Haynes <jackhaynes94@gmail.com>
2022-02-10 23:12:30 +0000

    Initial commit of Spring Initializr project.

---
+++ api-java/src/test/java/jackhaynes/cloudfib/api/CloudfibApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jackhaynes.cloudfib.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfibApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
